036a63edf6cf9b0b52d0520068acf0e580da561f
kuromi75@yandex.ru <kuromi75@yandex.ru>
2025-02-26 10:50:57 +0800

    Добавьте файлы проекта.

---
+++ laba1.1/laba1.1/laba1.1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+//Дано трехзначное число. Не прибегая к использованию строк вывести наибольшую из его цифр
+namespace FirstApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трехзначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            if (!Logica.IsThreeDigitNumber(number))
+            {
+                Console.WriteLine("Ошибка: Введите корректное трехзначное число.");
+                return;
+            }
+
+
+            int largestDigit = Logica.FindLargestDigit(number);
+
+
+            if (Logica.AllDigitsEqual(number))
+            {
+                Console.WriteLine("Все цифры одинаковы.");
+            }
+            else
+            {
+                Console.Write("Наибольшая цифра: " + largestDigit);
+            }
+        }
+    }
+
+    public class Logica
+    {
+
+        public static int FindLargestDigit(int number)
+        {
+            int a = number / 100; // первая цифра
+            int b = (number / 10) % 10; // вторая цифра
+            int c = number % 10; // третья цифра
+
+            int largestDigit = a;
+
+            // cравниваем 
+            if (b > largestDigit)
+            {
+                largestDigit = b;
+            }
+            if (c > largestDigit)
+            {
+                largestDigit = c;
+            }
+
+            return largestDigit;
+        }
+
+
+        public static bool IsThreeDigitNumber(int number)
+        {
+            if (number >= 100 && number <= 999)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool AllDigitsEqual(int number)
+        {
+            int a = number / 100; // первая цифра
+            int b = (number / 10) % 10; // вторая цифра
+            int c = number % 10; // третья цифра
+
+            return a == b && b == c;
+        }
+    }
+}
+//обязательно добавить проверку чтобы если например было число 222 то выводило больлшего числа нет
---
+++ laba1.1/laba1.1/laba1.1Tests/LogicaTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApp;
+
+namespace FirstApp.Tests
+{
+    [TestClass()]
+    public class LogicaTests
+    {
+        [TestMethod()]
+        public void TestOne()
+        {
+            var number = 354;
+            var result = Logica.FindLargestDigit(number);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod()]
+        public void TestTwo()
+        {
+            var number = 32;
+            var result = Logica.IsThreeDigitNumber(number);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void TestThree()
+        {
+            var number = 222;
+            var result = Logica.AllDigitsEqual(number);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void TestFour()
+        {
+            var number = 123;
+            var result = Logica.AllDigitsEqual(number);
+            Assert.IsFalse(result);
+        }
+
+    }
+}
